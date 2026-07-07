db444223ca8c4ed20f76c478c839f587c3156bc5
kenan <kenanmjamal@gmail.com>
2026-07-07 18:01:50 +0300

    finishing sorting and filtering api side

--- src/features/bookings/useBookings.js
+++ src/features/bookings/useBookings.js
@@ -1,14 +1,26 @@
 import { useQuery } from "@tanstack/react-query";
 import { getBookings } from "../../services/apiBookings";
+import { useSearchParams } from "react-router-dom";
 
 export function useBookings() {
+  const [searchParams] = useSearchParams();
+  //filter
+  const filterValue = searchParams.get("status");
+  const filter =
+    !filterValue || filterValue === "all"
+      ? null
+      : { field: "status", value: filterValue };
+  // sort
+  const sortValue = searchParams.get("sortBy") || "startDate-asc";
+  const [field, direction] = sortValue.split("-");
+  const sortBy = { field, direction };
   const {
     isLoading,
     data: bookings,
     error,
   } = useQuery({
-    queryKey: ["bookings"],
-    queryFn: getBookings,
+    queryKey: ["bookings", filter, sortBy],
+    queryFn: () => getBookings({ filter, sortBy }),
   });
   return { isLoading, bookings, error };
 }
--- src/pages/Bookings.jsx
+++ src/pages/Bookings.jsx
@@ -1,13 +1,14 @@
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
 import BookingsTable from "../features/bookings/BookingTable";
+import BookingsTableOperations from "../features/bookings/BookingTableOperations";
 
 function Bookings() {
   return (
     <>
       <Row type="horizontal">
         <Heading as="h1">All bookings</Heading>
-        <p>TEST</p>
+        <BookingsTableOperations />
       </Row>
       <BookingsTable />
     </>
