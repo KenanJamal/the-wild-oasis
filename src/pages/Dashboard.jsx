1b7930bb72880b706a7db60b960353670becc11f
kenan <kenanmjamal@gmail.com>
2026-09-21 12:16:29 +0300

    pla

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,12 +1,17 @@
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
+import DashBoardFilter from "../features/dashboard/DashBoardFilter";
+import DashBoardLayout from "../features/dashboard/DashBoardLayout";
 
 function Dashboard() {
   return (
-    <Row type="horizontal">
-      <Heading as="h1">Dashboard</Heading>
-      <p>TEST</p>
-    </Row>
+    <>
+      <Row type="horizontal">
+        <Heading as="h1">Dashboard</Heading>
+        <DashBoardFilter />
+      </Row>
+      <DashBoardLayout />
+    </>
   );
 }
 
